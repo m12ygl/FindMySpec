5676c4e6a1473023f9ef9f9fb4470971aca8ba5b
Yohann Ifrah <m12ygl@Yohanns-MacBook-Pro.local>
2026-05-06 12:31:40 +0100

    fix root in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!
+root.style.width = '100%'
+root.style.maxWidth = '100%'
+root.style.margin = '0'
+root.style.border = 'none'
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
